c8a1ffff4501a531a019384598cfafbceb3f9fe2
EragonpeerGynt <eragon.peergynt@gmail.com>
2020-12-05 12:42:42 +0000

    not my cleanest solution. I could probably make it more accurate and non dependant on existing good anwser, but what can I do. If there is no correct anwser second part will fail, but that should never happen

--- D05/solution.go
+++ D05/solution.go
@@ -2,8 +2,53 @@ package main
 
 import (
 	"AoC/Common/Reader"
+  "fmt"
+  "sort"
 )
 
 func main() {
-	reader.ReadAllLines()
+	input := reader.ReadLineArray()
+  fmt.Println(findHighestId(input))
+  fmt.Println(findMySeat(input))
+}
+
+func findHighestId(seattings []string) (max int) {
+  max = 0
+  var current int
+  for _,seatting := range seattings {
+    current = binaryConverter(seatting[:7]) * 8 + binaryConverter(seatting[7:])
+    if current > max {
+      max = current
+    }
+  }
+  return
+}
+
+func findMySeat(seattings []string) (seat int) {
+  var allSeats []int
+  for _,seatting := range seattings {
+    allSeats = append(allSeats, binaryConverter(seatting[:7]) * 8 + binaryConverter(seatting[7:]))
+  }
+
+  sort.Ints(allSeats)
+
+  for i,id := range allSeats {
+    if (id + 2 == allSeats[i+1]) {
+      seat = id + 1
+      return
+    }
+  }
+  return 0
+}
+
+func binaryConverter(binaryString string) (converted int) {
+  step := 1
+  converted = 0
+  for i := len(binaryString) - 1; i >= 0; i-- {
+    if (binaryString[i] == "B"[0] || binaryString[i] == "R"[0]) {
+      converted += step
+    }
+    step = step * 2
+  }
+  return
 }
